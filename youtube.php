cd21172f53be723f6bf9c6bad690bd20bd1651cd
xiatian0502 <108776938+xiatian0502@users.noreply.github.com>
2024-05-30 22:40:05 +0800

    Update youtube.php

--- youtube.php
+++ youtube.php
@@ -22,6 +22,7 @@ $playlistIds = [
     'PL0VLshn35eZIQ8eIvFcj0TiuiiVl08Jvf',    // 橘子老师国 外声乐老师超真实锐评
     'PLk3vQVLQ1uijSRpwOspimMhNAiT0qHUka'     // 锡兰Ceylan
 ];
+
 $maxResults = 20;
 $API_keys = [
     'AIzaSyAONZd3f8TN6QZS39WCeddl7YqP1TdhkkQ',
@@ -47,7 +48,7 @@ foreach ($playlistIds as $playlistId) {
     do {
         $API_key = getAPIKey($apiKeyIndex, $apiKeyCount, $API_keys);
         $api_url = 'https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&maxResults=' . $maxResults . '&playlistId=' . $playlistId . '&key=' . $API_key;
-        $videoListResponse = file_get_contents($api_url);
+        $videoListResponse = @file_get_contents($api_url);
         
         if ($videoListResponse === FALSE) {
             sleep(1); // 如果调用失败，等待1秒再重复
@@ -69,7 +70,7 @@ foreach ($playlistIds as $playlistId) {
 
         do {
             $API_key = getAPIKey($apiKeyIndex, $apiKeyCount, $API_keys);
-            $channelInfoResponse = file_get_contents('https://www.googleapis.com/youtube/v3/channels?part=snippet&id=' . $channelId . '&key=' . $API_key);
+            $channelInfoResponse = @file_get_contents('https://www.googleapis.com/youtube/v3/channels?part=snippet&id=' . $channelId . '&key=' . $API_key);
             
             if ($channelInfoResponse === FALSE) {
                 sleep(1); // 如果调用失败，等待1秒再重复
@@ -80,13 +81,15 @@ foreach ($playlistIds as $playlistId) {
         $channelTitle = isset($channelInfo['items'][0]['snippet']['title']) ? $channelInfo['items'][0]['snippet']['title'] : '未知频道';
 
         $command = "/home/runner/.local/bin/yt-dlp -f best --get-url --no-playlist --no-warnings --force-generic-extractor --user-agent 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:89.0) Gecko/20100101 Firefox/89.0' --youtube-skip-dash-manifest " . escapeshellarg($youtubeUrl);
-        $streamUrl = shell_exec($command);
-
-        if ($streamUrl !== null && strpos($streamUrl, 'http') === 0) {
-            $streamUrl = trim($streamUrl);
-        } else {
-            $streamUrl = $youtubeUrl;  // 使用原始URL
-        }
+        
+        do {
+            $streamUrl = shell_exec($command);
+            if ($streamUrl !== null && strpos($streamUrl, 'http') === 0) {
+                $streamUrl = trim($streamUrl);
+            } else {
+                sleep(1); // 如果调用失败，等待1秒再重复
+            }
+        } while (!isset($streamUrl) || strpos($streamUrl, 'http') !== 0);
 
         if (!isset($categories[$channelTitle])) {
             $categories[$channelTitle] = [];
@@ -94,7 +97,7 @@ foreach ($playlistIds as $playlistId) {
         $categories[$channelTitle][] = "#EXTINF:-1 group-title=\"" . $channelTitle . "\"," . $videoTitle . PHP_EOL . $streamUrl . PHP_EOL;
 
         // 延时以降低API调用频率，减少被限制的可能性
-        usleep(200000); // 200毫秒
+        usleep(250000); // 250毫秒
     }
 }
 
