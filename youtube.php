550c5d701bff800456e60fbc75168d45b0a8375a
xiatian0502 <108776938+xiatian0502@users.noreply.github.com>
2024-05-30 13:14:29 +0800

    Update youtube.php

--- youtube.php
+++ youtube.php
@@ -3,11 +3,30 @@
 $playlistIds = [
     'PLQWMqkNuwweK2NUFEex3Jked5lBWcUIJc&index=6',
     'PLNXJ_YC1PDA1L6H_ec0pn25QkDdK_8KrB',
-    'PLrVKx-I9kvCSllIRl4wpzLpJtUPUOSPb8',  // 原有播放列表ID
+    'PLrVKx-I9kvCRd869r_lVfnM6h4WHqdJe8',  // 原有播放列表ID
     'PLwACruPGUorXHJa2kX5Olh-YbKAb3rm-q',   // 新添加的播放列表ID
-    'PLQWMqkNuwweLu5hFpWCVHiRkdHhrHBpop'    // 新添加的播放列表ID
+    'PLQWMqkNuwweLu5hFpWCVHiRkdHhrHBpop',
+    'PLg4glStCiHhrb425xNCsu2pfBJvpvBVok',    // 帕太
+    'PLg4glStCiHhrICchD-I3W0z7FS7LEayu2',   // 帕太
+    'PLwACruPGUorWsIwJA6UetUpPu4qi6aUV2',   // 帕太
+    'PLrVKx-I9kvCTNaiwtXI3_VujjhZRYxAlM',    // 雷探长
+    'PLrVKx-I9kvCQN_yVvm2afr0_e-ES5K2tr',     // 雷探长
+    'PLrVKx-I9kvCTNZ0jKMUOqtnn5wNk13_Rq',    // 雷探长
+    'PLrVKx-I9kvCQ4gkJ-IGhhh2yZq38FobF8',    // 雷探长
+    'PLrVKx-I9kvCRAVwkSfzjq_eZ66f89sz_h',    // 雷探长
+    'PL0z1ZjXYEnlohS5MIDibsc1D8seHMwsXi',    // 雷探长
+    'PLiMeZxaUvGEJtAuzQMQxjSWJ6ueyEfTi4',      // 主播私享
+    'PLiMeZxaUvGEIZLpgh_9uZnDpSGp6Hg_61',      // 主播私享
+    'PLiMeZxaUvGEJtxtAyQVfkv_7QE9ttMNrz',     // 主播私享
+    'PLiMeZxaUvGEKRMoNvlvuowilo93bbCHvh',     // 主播私享
+    'PLPEKV0iMwYZLOkTmYFYxNzQnOo7aYzNiT',    // 老戴在此
+    'PLPEKV0iMwYZLIRBs8xSm9du4MoInhywy1',   // 老戴在此
+    'PLPEKV0iMwYZLUxFvziNn-q1lrLtyZKz_F',    // 老戴在此
+    'PLOrDt87s8A3rMMqrz_Sz2kSt4lzWiPg4h',    // 李永乐老师
+    'PL0VLshn35eZIQ8eIvFcj0TiuiiVl08Jvf',    // 橘子老师國外聲樂老師超真實銳評
+    'PLk3vQVLQ1uijSRpwOspimMhNAiT0qHUka'    // 錫蘭Ceylan
 ];
-$maxResults = 20;
+$maxResults = 30;
 $API_key = 'AIzaSyAONZd3f8TN6QZS39WCeddl7YqP1TdhkkQ'; 
 
 $categories = [];
