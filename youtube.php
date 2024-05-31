3922bd1b05333822e421a54812ff19471861eb86
xiatian0502 <108776938+xiatian0502@users.noreply.github.com>
2024-05-31 08:08:59 +0800

    Update youtube.php

--- youtube.php
+++ youtube.php
@@ -78,24 +78,29 @@ foreach ($playlistIds as $playlistId) {
         $channelInfo = makeRequest($channelApiUrl);
         $channelTitle = isset($channelInfo['items'][0]['snippet']['title']) ? $channelInfo['items'][0]['snippet']['title'] : '未知频道';
 
-        $command = "/home/runner/.local/bin/yt-dlp -f best --get-url --no-playlist --no-warnings --force-generic-extractor --user-agent 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:89.0) Gecko/20100101 Firefox/89.0' --youtube-skip-dash-manifest " . escapeshellarg($youtubeUrl);
+        // 确保 URL 有效
+        if (filter_var($youtubeUrl, FILTER_VALIDATE_URL)) {
+            $command = "/home/runner/.local/bin/yt-dlp -f best --get-url --no-playlist --no-warnings --force-generic-extractor --user-agent 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:89.0) Gecko/20100101 Firefox/89.0' --youtube-skip-dash-manifest " . escapeshellarg($youtubeUrl);
 
-        do {
-            $streamUrl = shell_exec($command);
-            if ($streamUrl !== null && strpos($streamUrl, 'http') === 0) {
-                $streamUrl = trim($streamUrl);
-            } else {
-                usleep(600000); // 如果调用失败，等待 0.6 秒再重复
+            do {
+                $streamUrl = shell_exec($command);
+                if ($streamUrl !== null && strpos($streamUrl, 'http') === 0) {
+                    $streamUrl = trim($streamUrl);
+                } else {
+                    usleep(500000); // 如果调用失败，等待 0.5 秒再重复
+                }
+            } while (!isset($streamUrl) || strpos($streamUrl, 'http') !== 0);
+
+            if (!isset($categories[$channelTitle])) {
+                $categories[$channelTitle] = [];
             }
-        } while (!isset($streamUrl) || strpos($streamUrl, 'http') !== 0);
-
-        if (!isset($categories[$channelTitle])) {
-            $categories[$channelTitle] = [];
+            $categories[$channelTitle][] = "#EXTINF:-1 group-title=\"" . $channelTitle . "\"," . $videoTitle . PHP_EOL . $streamUrl . PHP_EOL;
+        } else {
+            error_log("Invalid URL: $youtubeUrl");
         }
-        $categories[$channelTitle][] = "#EXTINF:-1 group-title=\"" . $channelTitle . "\"," . $videoTitle . PHP_EOL . $streamUrl . PHP_EOL;
 
         // 延时以降低API调用频率，减少被限制的可能性
-        usleep(600000); // 600毫秒
+        usleep(500000); // 500毫秒
     }
 }
 
